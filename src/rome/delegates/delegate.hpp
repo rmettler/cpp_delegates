667a3a940ba9c43f090a618bfd789ecf41b32436
Roger Mettler <roger@roger-mettler.ch>
2019-02-26 00:54:00 +0100

    updated licenses

--- src/rome/delegates/delegate.hpp
+++ src/rome/delegates/delegate.hpp
@@ -1,40 +1,17 @@
-/*
- * Created: 2019-02-24
- * Author: Roger Mettler (roger@roger-mettler.ch)
- * -----
- * Last Modified: 2019-02-25 01:26:04
- * Modified By: Roger Mettler
- * -----
- * Copyright (c) 2019 Roger Mettler All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+// 
+// Copyright Roger Mettler 2019.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// https://www.boost.org/LICENSE_1_0.txt)
+// 
+
 
 namespace rome {
 namespace delegates {
 // TODO add a proper .clang-format!!!
 // TODO check whether is function, member, static or non-static
 //      std::is_function -> all functions
+//      std::is_member_pointer, is_member_object_pointer, is_member_function_pointer
 template <typename T> struct delegate;
 
 template <typename Ret, typename... Args> struct delegate<Ret(Args...)> {
