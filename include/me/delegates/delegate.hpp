ac4f9b4489d53f00e554541e618d8f4588c7a5dc
Roger Mettler <roger@roger-mettler.ch>
2019-03-03 22:06:48 +0100

    update to support function and methods and fully work during compile time

--- include/me/delegates/delegate.hpp
+++ include/me/delegates/delegate.hpp
@@ -1,37 +1,145 @@
-// 
+//
+// Project: delegates
+// TODO: file description
+//
+// Based on the article of Sergey Ryazanov:
+// "The Impossibly Fast C++ Delegates", 18 Jul 2005
+// https://www.codeproject.com/articles/11015/the-impossibly-fast-c-delegates
+//
 // Copyright Roger Mettler 2019.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // https://www.boost.org/LICENSE_1_0.txt)
-// 
+//
 
+#pragma once
+
+#include <cstddef>
 
 namespace me {
 namespace delegates {
-// TODO add a proper .clang-format!!!
-// TODO check whether is function, member, static or non-static
+// TODO: add a proper .clang-format!!!
+// TODO: check whether is function, member, static or non-static
 //      std::is_function -> all functions
-//      std::is_member_pointer, is_member_object_pointer, is_member_function_pointer
-template <typename T> struct delegate;
+//      std::is_member_pointer, is_member_object_pointer,
+//      is_member_function_pointer
+// TODO: compare with other implementations
+// TODO: split implementations and create a delegates.h file including all of
+// them
+// TODO: enhance the delegate factory
+// TODO: is "non static member" the correct name? or are they only non static
+// methods?
 
-template <typename Ret, typename... Args> struct delegate<Ret(Args...)> {
-    // TODO enhance by passing the stub as template argument (or defaulting to a dynamic one?)
-    /* data */
+template <typename T> class delegate;
+
+template <typename Ret, typename... Args> class delegate<Ret(Args...)> {
+  public:
+    using signature_type = Ret(Args...);
+    using caller_type = Ret (*)(void *, Args...);
+
+    constexpr delegate() = default;
+    constexpr delegate(const delegate &orig) = default;
+    constexpr delegate(delegate &&) = default;
+    constexpr delegate(const std::nullptr_t &) : delegate() {}
+
+    constexpr delegate &operator=(const delegate &orig) = default;
+    constexpr delegate &operator=(delegate &&) = default;
+    constexpr delegate &operator=(const std::nullptr_t &)
+    {
+        obj_ = nullptr;
+        caller_ = nullptr;
+    }
+
+    constexpr Ret operator()(Args... args) const
+    {
+        return caller_(obj_, args...);
+    }
+
+    constexpr bool isSet() const { return caller_ != nullptr; }
+    constexpr bool operator!() const { return !isSet(); }
+    constexpr operator bool() const { return isSet(); }
+
+    template <typename C, Ret (C::*pMethod)(Args...)>
+    constexpr static delegate create_from_non_static_member(C *obj)
+    {
+        delegate d;
+        d.obj_ = obj;
+        d.caller_ = (obj != nullptr) ? &method_call<C, pMethod> : nullptr;
+        return d;
+    }
+
+    template <typename C, Ret (C::*pMethod)(Args...) const>
+    constexpr static delegate create_from_non_static_const_member(C const *obj)
+    {
+        delegate d;
+        d.obj_ = const_cast<C *>(obj);
+        d.caller_ = (obj != nullptr) ? &const_method_call<C, pMethod> : nullptr;
+        return d;
+    }
+
+    template <Ret (*pFunction)(Args...)>
+    constexpr static delegate create_from_function()
+    {
+        delegate d;
+        d.obj_ = nullptr;
+        d.caller_ = &function_call<pFunction>;
+        return d;
+    }
+
+  private:
+    template <typename C, Ret (C::*pMethod)(Args...)>
+    constexpr static Ret method_call(void *obj, Args... args)
+    {
+        return (static_cast<C *>(obj)->*pMethod)(args...);
+    }
+
+    template <typename C, Ret (C::*pMethod)(Args...) const>
+    constexpr static Ret const_method_call(void *obj, Args... args)
+    {
+        return (static_cast<C const *>(obj)->*pMethod)(args...);
+    }
+
+    template <Ret (*pFunction)(Args...)>
+    constexpr static Ret function_call(void *, Args... args)
+    {
+        return (*pFunction)(args...);
+    }
+
+    void *obj_ = nullptr;
+    caller_type caller_ = nullptr;
 };
 
-template <typename T, T t> struct delegate_creator;
+template <typename T, T t, typename Enable = void> struct delegate_factory;
 
-template <typename Ret, typename... Args, typename C, Ret (C::*pMem)(Args...)>
-struct delegate_creator<Ret (C::*)(Args...), pMem> {
-    static constexpr auto create(C *)
+template <typename C, typename Ret, typename... Args, Ret (C::*pMem)(Args...)>
+struct delegate_factory<Ret (C::*)(Args...), pMem> {
+    constexpr static auto create(C *obj)
     {
-        // TODO create the specific type
-        return delegate<Ret(Args...)>{};
+        return delegate<Ret(
+            Args...)>::template create_from_non_static_member<C, pMem>(obj);
+    }
+};
+
+template <typename C, typename Ret, typename... Args,
+          Ret (C::*pMem)(Args...) const>
+struct delegate_factory<Ret (C::*)(Args...) const, pMem> {
+    constexpr static auto create(C const *obj)
+    {
+        return delegate<Ret(Args...)>::
+            template create_from_non_static_const_member<C, pMem>(obj);
+    }
+};
+
+template <typename Ret, typename... Args, Ret (*pMem)(Args...)>
+struct delegate_factory<Ret (*)(Args...), pMem> {
+    constexpr static auto create()
+    {
+        return delegate<Ret(Args...)>::template create_from_function<pMem>();
     }
 };
 
 template <typename T, T t>
-constexpr auto make_delegate = delegate_creator<T, t>::create;
+constexpr auto make_delegate = delegate_factory<T, t>::create;
 
 } // namespace delegates
 } // namespace me
--- test/test_delegate.cpp
+++ test/test_delegate.cpp
@@ -1,37 +1,148 @@
-// 
+//
 // Copyright Roger Mettler 2019.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // https://www.boost.org/LICENSE_1_0.txt)
-// 
+//
 
 #include "me/delegates/delegate.hpp"
+#include <cassert>
 
 namespace me {
 
 struct Functor {
-    bool operator()(int) { return false; }
+    int operator()(bool) { return 1; }
 };
 
 struct A {
-    bool foo(int) { return false; }
-    bool cfoo(int) const { return false; }
-    static bool sfoo(int) { return false; }
+    int foo(bool) { return 2; }
+    constexpr int cefoo(bool) { return 12; }
+    int cfoo(bool) const { return 3; }
+    constexpr int cecfoo(bool) const { return 13; }
+    static int sfoo(bool) { return 4; }
+    constexpr static int cesfoo(bool) { return 14; }
     Functor functor;
-    bool (A::*pFoo)(int) = foo;
-    bool (A::*pCFoo)(int) const = cfoo;
-    bool (*pSFoo)(int) = sfoo;
-    bool (&rSFoo)(int) = sfoo;
+    int (A::*pFoo)(bool) = foo;
+    int (A::*pCFoo)(bool) const = cfoo;
+    int (*pSFoo)(bool) = sfoo;
+    int (&rSFoo)(bool) = sfoo;
 };
 
-bool foo(int) { return false; }
+int foo(bool) { return 5; }
+constexpr int cefoo(bool) { return 15; }
 
-A a;
+static constexpr A a;
+
+namespace test_not_set {
+static constexpr delegates::delegate<int(bool)> implcitlyNotSet{};
+static_assert(false == implcitlyNotSet.isSet(), "");
+static_assert(false == implcitlyNotSet, "");
+static_assert(true == !implcitlyNotSet, "");
+
+static constexpr delegates::delegate<int(bool)> explicitlyNotSet{nullptr};
+static_assert(false == explicitlyNotSet.isSet(), "");
+static_assert(false == explicitlyNotSet, "");
+static_assert(true == !explicitlyNotSet, "");
+} // namespace test_not_set
+
+namespace test_constexpr_method {
+static constexpr auto direct =
+    delegates::delegate<int(bool)>::create_from_non_static_member<A, &A::cefoo>(
+        const_cast<A *>(&a));
+static_assert(12 == direct(true), "");
+static_assert(true == direct.isSet(), "");
+static_assert(true == direct, "");
+static_assert(false == !direct, "");
+
+static constexpr auto factory =
+    delegates::make_delegate<decltype(&A::cefoo), &A::cefoo>(
+        const_cast<A *>(&a));
+static_assert(12 == factory(true), "");
+static_assert(true == factory.isSet(), "");
+static_assert(true == factory, "");
+static_assert(false == !factory, "");
+
+static constexpr auto directNullObj =
+    delegates::delegate<int(bool)>::create_from_non_static_member<A, &A::cefoo>(
+        nullptr);
+static_assert(false == directNullObj.isSet(), "");
+static_assert(false == directNullObj, "");
+static_assert(true == !directNullObj, "");
+
+static constexpr auto factoryNullObj =
+    delegates::make_delegate<decltype(&A::cefoo), &A::cefoo>(nullptr);
+static_assert(false == factoryNullObj.isSet(), "");
+static_assert(false == factoryNullObj, "");
+static_assert(true == !factoryNullObj, "");
+} // namespace test_constexpr_method
+
+namespace test_constexpr_const_method {
+static constexpr auto direct = delegates::delegate<int(
+    bool)>::create_from_non_static_const_member<A, &A::cecfoo>(&a);
+static_assert(13 == direct(true), "");
+static_assert(true == direct.isSet(), "");
+static_assert(true == direct, "");
+static_assert(false == !direct, "");
+
+static constexpr auto factory =
+    delegates::make_delegate<decltype(&A::cecfoo), &A::cecfoo>(&a);
+static_assert(13 == factory(true), "");
+static_assert(true == factory.isSet(), "");
+static_assert(true == factory, "");
+static_assert(false == !factory, "");
+
+// Passing a nullptr as object lead to a non constexpr call and throw a compile
+// error (GCC 8.2). Hence these tests are omitted.
+} // namespace test_constexpr_const_method
+
+namespace test_constexpr_static_method {
+static constexpr auto direct =
+    delegates::delegate<int(bool)>::create_from_function<&A::cesfoo>();
+static_assert(14 == direct(true), "");
+static_assert(true == direct.isSet(), "");
+static_assert(true == direct, "");
+static_assert(false == !direct, "");
+
+static constexpr auto factory =
+    delegates::make_delegate<decltype(&A::cesfoo), &A::cesfoo>();
+static_assert(14 == factory(true), "");
+static_assert(true == factory.isSet(), "");
+static_assert(true == factory, "");
+static_assert(false == !factory, "");
+} // namespace test_constexpr_static_method
+
+namespace test_constexpr_function {
+static constexpr auto direct =
+    delegates::delegate<int(bool)>::create_from_function<&cefoo>();
+static_assert(15 == direct(true), "");
+static_assert(true == direct.isSet(), "");
+static_assert(true == direct, "");
+static_assert(false == !direct, "");
+
+static constexpr auto factory =
+    delegates::make_delegate<decltype(&cefoo), &cefoo>();
+static_assert(15 == factory(true), "");
+static_assert(true == factory.isSet(), "");
+static_assert(true == factory, "");
+static_assert(false == !factory, "");
+} // namespace test_constexpr_function
+
+// TODO: test class members which are callable but no functions (functor,
+// references of functions)
+// TODO: test namespace variables which are callable but no functions (functor,
+// references of functions)
+// TODO: test with fancy arguments (modifiers, lvalue and rvalue references, non
+// constexpr arguments)
+// TODO: test with fancy return types (modifiers, lvalue and rvalue references,
+// non constexpr types)
+// TODO: test with void return and no or multiple arguments
+// TODO: test usage of lambdas
 
 void runTests()
 {
-    delegates::delegate<bool(int)> d =
-        delegates::make_delegate<decltype(A::foo), A::foo>(&a);
+    // TODO: test dynamic usage of all the above delegates
+    // TODO: extend test for setting and unsetting of all the delegates
+    // (set one, set another or set one, unset, then set another)
 }
 
 } // namespace me
