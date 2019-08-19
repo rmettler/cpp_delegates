8775e1c91f0a753dba29e48defe2f35240482e62
Roger Mettler <roger@roger-mettler.ch>
2019-08-19 22:21:36 +0200

    add empty test for rome::bad_delegate_call

---
+++ test/test_bad_delegate_call.cpp
@@ -0,0 +1,10 @@
+#include <doctest.h>
+
+TEST_SUITE_BEGIN("header file: rome/bad_delegate_call.hpp");
+
+TEST_CASE("exception: rome::bad_delegate_call" * doctest::skip()) {
+    // TODO: implement testing the exception!
+    CHECK(false);
+}
+
+TEST_SUITE_END();  // rome/bad_delegate_call.hpp
