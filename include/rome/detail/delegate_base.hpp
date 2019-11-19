8bb346ad6d7d56ddaf990e890f382586fc148ee0
Roger Mettler <roger@roger-mettler.ch>
2019-11-19 23:22:42 +0100

    remove UB const_cast from delegate_base

--- include/rome/detail/delegate_base.hpp
+++ include/rome/detail/delegate_base.hpp
@@ -21,8 +21,8 @@ namespace detail {
 
         using buffer_type = void*;
         template<typename Ret, typename... Args>
-        using invoker_type = Ret (*)(buffer_type const&, Args&&...);
-        using deleter_type = void (*)(buffer_type const&);
+        using invoker_type = Ret (*)(buffer_type&, Args&&...);
+        using deleter_type = void (*)(buffer_type&);
 
         static constexpr auto bufferAlignment() {
             return std::max(sizeof(buffer_type), alignof(buffer_type));
@@ -37,23 +37,16 @@ namespace detail {
         // Used as template parameter for delegate_base. Calls the provided invoke function when
         // empty.
         struct no_call_invoker {
-            template<typename Ret, typename... Args/*,
-                std::enable_if_t<std::is_same<Ret, void>::value, int> = 0*/>
-            static inline Ret invoke(buffer_type const&, Args&&...) {
+            template<typename Ret, typename... Args>
+            static inline Ret invoke(buffer_type&, Args&&...) {
             }
-            /*template<typename Ret, typename... Args,
-                std::enable_if_t<!std::is_same<Ret, void>::value, int> = 0>
-            static Ret invoke(buffer_type const&, Args&&...);*/
-            // TODO: fix this! find a workaround for when optional is used with Ret != void and all
-            // the templates are instatiated...
-            //! probably at best replace std::is_base_of since it seems to instantiate the whole
-            //! template!
         };
+
         // Used as template parameter for delegate_base. Calls the provided invoke function when
         // empty.
         struct exception_call_invoker {
             template<typename Ret, typename... Args>
-            [[noreturn]] static inline Ret invoke(buffer_type const&, Args&&...) {
+            [[noreturn]] static inline Ret invoke(buffer_type&, Args&&...) {
 #if (defined(__cpp_exceptions) || defined(__EXCEPTIONS) || defined(_CPPUNWIND))
                 throw rome::bad_delegate_call{};
 #else
@@ -61,12 +54,15 @@ namespace detail {
 #endif
             }
         };
+
+        // Used as template parameter for delegate_base. Sets nullptr as the function pointer. Is
+        // only used for invalid template parameter combinations.
         struct nullptr_invoker {
             static constexpr auto invoke = nullptr;
         };
 
         // Used as deleter in delegate_base when no destruction of buffer is needed.
-        inline void no_delete(buffer_type const&) {
+        inline void no_delete(buffer_type&) {
         }
 
         template<typename Signature, typename EmptyInvoker>
@@ -75,17 +71,15 @@ namespace detail {
         template<typename Ret, typename... Args, typename EmptyInvoker>
         class delegate_base<Ret(Args...), EmptyInvoker> {
           private:
-            // TODO: update types to be big enough for all used pointers!
-            // TODO: remove const_cast and replace it with mutable buffer! (add const later)
             // buffer_ needs to be writable by 'operator()(Args...) const' if small buffer
             // optimization is used
-            alignas(bufferAlignment()) buffer_type buffer_ = nullptr;
+            alignas(bufferAlignment()) mutable buffer_type buffer_ = nullptr;
             invoker_type<Ret, Args...> callee_                     = EmptyInvoker::invoke;
             deleter_type deleter_                                  = no_delete;
 
           public:
             constexpr delegate_base() noexcept           = default;
-            delegate_base(delegate_base const&) noexcept = delete;
+            delegate_base(const delegate_base&) noexcept = delete;
             delegate_base(delegate_base&& orig) noexcept {
                 orig.swap(*this);
             }
@@ -94,7 +88,7 @@ namespace detail {
                 (*deleter_)(buffer_);
             }
 
-            delegate_base& operator=(delegate_base const&) noexcept = delete;
+            delegate_base& operator=(const delegate_base&) noexcept = delete;
             delegate_base& operator=(delegate_base&& orig) noexcept {
                 delegate_base{std::move(orig)}.swap(*this);
                 return *this;
@@ -125,7 +119,7 @@ namespace detail {
             constexpr static delegate_base create() noexcept {
                 delegate_base d;
                 d.buffer_ = nullptr;
-                d.callee_ = [](buffer_type const&, Args&&... args) -> Ret {
+                d.callee_ = [](buffer_type&, Args&&... args) -> Ret {
                     return (*function)(std::forward<Args>(args)...);
                 };
                 d.deleter_ = no_delete;
@@ -138,7 +132,7 @@ namespace detail {
             static delegate_base create(C& obj) noexcept {
                 delegate_base d;
                 d.buffer_ = &obj;
-                d.callee_ = [](buffer_type const& buffer, Args&&... args) -> Ret {
+                d.callee_ = [](buffer_type& buffer, Args&&... args) -> Ret {
                     return (static_cast<C*>(buffer)->*method)(std::forward<Args>(args)...);
                 };
                 d.deleter_ = no_delete;
@@ -151,7 +145,7 @@ namespace detail {
             static delegate_base create(const C& obj) noexcept {
                 delegate_base d;
                 d.buffer_ = const_cast<C*>(&obj);  // const will be added again before used
-                d.callee_ = [](buffer_type const& buffer, Args&&... args) -> Ret {
+                d.callee_ = [](buffer_type& buffer, Args&&... args) -> Ret {
                     return (static_cast<const C*>(buffer)->*method)(std::forward<Args>(args)...);
                 };
                 d.deleter_ = no_delete;
@@ -164,25 +158,16 @@ namespace detail {
                 std::enable_if_t<isSmallBufferOptimizable<Invokable>(), int> = 0>
             static delegate_base create(Invokable invokable) noexcept(
                 std::is_nothrow_move_constructible<Invokable>::value) {
-                // Reason for using const_cast here:
-                // The functor needs be able to change its own members. Here, the functor and
-                // its members are embedded in delegate_base::buffer_ using small buffer
-                // optimization. Thus the functor needs to be able to change buffer_, even so
-                // the delegate_base doesn't change its behavior.
-                //! TODO: might lead to an error! when:
-                //!   static const delegate d{delegate::create(bla)};
-                //!   d(); // and this changes the delegate, which might be in RO memory!
-                //! maybe set buffer_ mutable?
-                //! maybe buffer_type const& can be replaced too with buffer_type&!
                 delegate_base d;
                 const auto ptr = ::new (&d.buffer_) Invokable(std::move(invokable));
                 assert(ptr == static_cast<Invokable*>(static_cast<void*>(&d.buffer_)));
-                d.callee_ = [](buffer_type const& buffer, Args&&... args) -> Ret {
-                    auto pFunctor = static_cast<void*>(const_cast<buffer_type*>(&buffer));
-                    return (*static_cast<Invokable*>(pFunctor))(std::forward<Args>(args)...);
+                d.callee_ = [](buffer_type& buffer, Args&&... args) -> Ret {
+                    auto pFunctor = static_cast<void*>(&buffer);
+                    return static_cast<Invokable*>(pFunctor)->operator()(
+                        std::forward<Args>(args)...);
                 };
-                d.deleter_ = [](buffer_type const& buffer) -> void {
-                    auto pFunctor = static_cast<void*>(const_cast<buffer_type*>(&buffer));
+                d.deleter_ = [](buffer_type& buffer) -> void {
+                    auto pFunctor = static_cast<void*>(&buffer);
                     static_cast<Invokable*>(pFunctor)->~Invokable();
                 };
                 return d;
@@ -195,10 +180,10 @@ namespace detail {
             static delegate_base create(Invokable invokable) {
                 delegate_base d;
                 d.buffer_ = new Invokable(std::move(invokable));
-                d.callee_ = [](buffer_type const& buffer, Args&&... args) -> Ret {
-                    return (*static_cast<Invokable*>(buffer))(std::forward<Args>(args)...);
+                d.callee_ = [](buffer_type& buffer, Args&&... args) -> Ret {
+                    return static_cast<Invokable*>(buffer)->operator()(std::forward<Args>(args)...);
                 };
-                d.deleter_ = [](buffer_type const& buffer) -> void {
+                d.deleter_ = [](buffer_type& buffer) -> void {
                     delete static_cast<Invokable*>(buffer);
                 };
                 return d;
